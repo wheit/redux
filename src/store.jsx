07b2ce07d6df8f874797c5747ce2f8c6fdff1bdd
Andrei Boboc <wheitpgla@gmail.com>
2026-01-23 17:36:07 +0200

    first commit

---
+++ src/store.jsx
@@ -0,0 +1,59 @@
+import { createStore } from "redux";
+
+const initalState = {
+  balance: 0,
+  loan: 0,
+  loadPurpose: "",
+};
+
+function reducer(state = initalState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+}
+const store = createStore(reducer);
+// store.dispatch({ type: "account/deposit", payload: 500 });
+// store.dispatch({ type: "account/withdraw", payload: 200 });
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 1000, purpose: "Buy a car" },
+// });
+// console.log(store.getState());
+// store.dispatch({ type: "account/payLoan" });
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+function requestLoan(amount, purpose) {
+  return { type: "account/requestLoan", payload: { amount, purpose } };
+}
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+store.dispatch(deposit(500));
+store.dispatch(requestLoan(1000, "buy a block"));
+store.dispatch(payLoan());
+console.log(store.getState());
